2e09131d1f9adf2f1e16e04fd37cc9b218fc8865
Nikki Verhulp <Nikki@lindas-mbp.lan>
2022-07-18 22:37:37 -0700

    updated javascript

--- Develop/script.js
+++ Develop/script.js
@@ -3,15 +3,6 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate')
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 // Function to generate password
 function generatePassword() {
 
@@ -73,6 +64,15 @@ for (var i = 0; i < passwordLength; i++) {
 return userArray.join("");
 }
 generatePassword()
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
 
 
 // Add event listener to generate button
