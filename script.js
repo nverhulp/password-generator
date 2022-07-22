57c86df25decbcbc1f4a78149238dccb81603bf3
Nikki Verhulp <Nikki@lindas-mbp.lan>
2022-07-21 19:01:09 -0700

    deleted generatePassword()

--- script.js
+++ script.js
@@ -17,53 +17,52 @@ function generatePassword() {
   var userArray = [];
 
 
- // Ask how many characters for password
- function charLength () {
-  passwordLength = prompt("How many characters would you like your password? Choose a value between 8 and 128.");
- if (passwordLength < 8 || passwordLength > 128 || passwordLength == "") {
-  // If user inputs value outside range, error message will appear
-  alert("Error! Please input a value that is between 8 and 128.");
-  charLength();
- }
-}
-charLength()
-
-// User must select at least one of the following password options
-function passwordOptions() {
-  wantsLowercase = confirm ("Do you want to include lowercase characters?");
-  wantsUppercase = confirm ("Do you want to include UPPERCASE characters?");
-  wantsNumbers = confirm ("Do you want to include numbers?");
-  wantsSpecial = confirm ("Do you want to include special characters?");
-// If user does not select at least one option, error message will appear
-  if (wantsLowercase == false && wantsUppercase == false && wantsNumbers == false && wantsSpecial == false) {
-    alert("Error! Please select at least one option.");
-    passwordOptions();
+  // Ask how many characters for password
+  function charLength() {
+    passwordLength = prompt("How many characters would you like your password? Choose a value between 8 and 128.");
+    if (passwordLength < 8 || passwordLength > 128 || passwordLength == "") {
+      // If user inputs value outside range, error message will appear
+      alert("Error! Please input a value that is between 8 and 128.");
+      charLength();
+    }
   }
-}
-passwordOptions()
+  charLength()
 
-// Condition of the arrays
-if (wantsLowercase) {
-  resultArray = resultArray.concat(lowercaseSet);
-}
-if (wantsUppercase) {
-  resultArray = resultArray.concat(uppercaseSet);
-}
-if (wantsNumbers) {
-  resultArray = resultArray.concat(numberSet);
-}
-if (wantsSpecial) {
-  resultArray = resultArray.concat(specialcharSet);
-}
-console.log(resultArray)
+  // User must select at least one of the following password options
+  function passwordOptions() {
+    wantsLowercase = confirm("Do you want to include lowercase characters?");
+    wantsUppercase = confirm("Do you want to include UPPERCASE characters?");
+    wantsNumbers = confirm("Do you want to include numbers?");
+    wantsSpecial = confirm("Do you want to include special characters?");
+    // If user does not select at least one option, error message will appear
+    if (wantsLowercase == false && wantsUppercase == false && wantsNumbers == false && wantsSpecial == false) {
+      alert("Error! Please select at least one option.");
+      passwordOptions();
+    }
+  }
+  passwordOptions()
 
-// This is what generates a password
-for (var i = 0; i < passwordLength; i++) {
-  userArray.push (resultArray[Math.floor(Math.random() * resultArray.length)]); 
+  // Condition of the arrays
+  if (wantsLowercase) {
+    resultArray = resultArray.concat(lowercaseSet);
+  }
+  if (wantsUppercase) {
+    resultArray = resultArray.concat(uppercaseSet);
+  }
+  if (wantsNumbers) {
+    resultArray = resultArray.concat(numberSet);
+  }
+  if (wantsSpecial) {
+    resultArray = resultArray.concat(specialcharSet);
+  }
+  console.log(resultArray)
+
+  // This is what generates a password
+  for (var i = 0; i < passwordLength; i++) {
+    userArray.push(resultArray[Math.floor(Math.random() * resultArray.length)]);
+  }
+  return userArray.join("");
 }
-return userArray.join("");
-}
-generatePassword()
 
 // Write password to the #password input
 function writePassword() {
